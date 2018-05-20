d2e0a2e157ee26583322fc9214878e8de5685595
Raymond Leon <raymondleon@raymondleon.ca>
2018-05-20 14:42:40 -0400

    Fixed some bugs.

--- image_grow.js
+++ image_grow.js
@@ -4,14 +4,13 @@ function image_grow(){
       var percentage = 0
       var inner_function = function(){
         percentage = percentage + 1
-        console.log(percentage)
         if (percentage >= 100){
           clearInterval(getClearIntervalVariableFunction())
         }
-
-        image_to_grow.height = image_to_grow.naturalHeight * (percentage / 100.0)
-        image_to_grow.width = image_to_grow.naturalWidth * (percentage / 100.0)
+        image_to_grow.style.height = image_to_grow.naturalHeight * (percentage / 100.0)
+        image_to_grow.style.width = image_to_grow.naturalWidth * (percentage / 100.0)
       }
+
       return inner_function
     }
 
@@ -31,43 +30,13 @@ function image_grow(){
 
   var images=document.getElementsByTagName('img')
   for(var i = 0; i < images.length; i++){
+    images[i].style.width = 0
+    images[i].style.height = 0
+  }
+
+  for(var i = 0; i < images.length; i++){
     images[i].onload = do_stuff_function(images[i])
   }
 }
 
-function grow_image(image_to_grow){
-  return function(){
-    image_to_grow.style.display = 'block'
-    image_to_grow.height +=1
-    image_to_grow.width +=1
-  }
-}
 
-//takes in an html element and grows it from its
-function grow_putty_grow(image){
-  image.height = 0
-  image.width = 0
-  image.style.display = 'block'
-
-  var closure = function(){
-    var inner_image = image
-    var percentage = 0
-    var inner_function = function(){
-      console.log(percentage)
-      if (percentage == 100){
-        //stop growing
-        clearTimeout(inner_image.growing_function)
-      }
-      else{
-        percentage = percentage + 1
-        image.height = image.naturalHeight * (percentage /100.0)
-        image.width = image.naturalWidth * (percentage /100.0)
-      }
-    }
-    return inner_function
-  }()
-
-  image.growing_function = setTimeout(
-    closure, 1
-  );
-}
